e3e5a129eb6f3869f1c6c32bf810438747e86d88
Henrico <henrico.almeida@fcamara.com.br>
2020-02-21 12:22:55 -0300

    refactor: make SignUpController conforms to a protocol

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,5 +1,5 @@
 import { SignUpController } from './signup'
-import { MissingParamError } from '../erros/missing-param-error'
+import { MissingParamError } from '../errors/missing-param-error'
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
