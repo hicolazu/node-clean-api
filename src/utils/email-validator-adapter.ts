e9ffaa2dea3fd640c0fb3cb4fb03afd927ede91a
Henrico <henrico.almeida@fcamara.com.br>
2020-03-03 16:12:01 -0300

    refactor: solve indentation problems according to eslint and prettier config

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -2,7 +2,7 @@ import { EmailValidator } from '../presentation/protocols/email-validator'
 import validator from 'validator'
 
 export class EmailValidatorAdapter implements EmailValidator {
-    isValid(email: string): boolean {
-        return validator.isEmail(email);
-    }
+  isValid (email: string): boolean {
+    return validator.isEmail(email)
+  }
 }
