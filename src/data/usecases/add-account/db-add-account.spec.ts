df9e65235b73059dab0a282b59e2489bfbacebaa
Henrico <hicolazu22@hotmail.com>
2020-03-05 15:50:02 -0300

    test: create db-add-account interfaces

---
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -0,0 +1,31 @@
+import { DbAddAccount } from './db-add-account'
+import { Encrypter } from '../../protocols/encrypter';
+
+describe('DbAddAccount Usecase', () => {
+    const makeSut = (encrypterStub) => {
+        return new DbAddAccount(encrypterStub)
+    }
+
+    const makeEncrypterStub = () => {
+        class EncrypterStub implements Encrypter{
+            async encrypt (value: string): Promise<string> {
+                return new Promise(resolve => resolve('hashed_password'))
+            }
+        }
+
+        return new EncrypterStub()
+    }
+
+    test('Should call encrypter with correct password', async () => {
+        const encrypterStub = makeEncrypterStub()
+        const sut = makeSut(encrypterStub)
+        const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
+        const accountData = {
+            name: "valid_name",
+            email: "valid_email",
+            password: "valid_password"
+        }
+        await sut.add(accountData)
+        expect(encryptSpy).toHaveBeenCalledWith('valid_password')
+    });
+});
